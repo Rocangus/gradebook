2c0f22fd993bf077c5d9d25c6ead5c4685b4e5fc
Rocangus <hunter_sniper112@hotmail.com>
2019-10-22 15:22:27 +0200

    Fundamentals 7: Pattern Matching with switch

--- test/GradeBookTest/BookTests.cs
+++ test/GradeBookTest/BookTests.cs
@@ -36,6 +36,7 @@ namespace GradeBookTest
             Assert.Equal(85.6, result.Average, 1);
             Assert.Equal(90.5, result.High, 1);
             Assert.Equal(77.3, result.Low, 1);
+            Assert.Equal('B', result.Letter);
         }
     }
 }
