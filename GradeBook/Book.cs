561c221869673b955b4b9513399d45275b7db7dd
Rocangus <hunter_sniper112@hotmail.com>
2019-10-22 14:40:30 +0200

    Initial upload

---
+++ GradeBook/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public class Book
+    {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            Name = name;
+        }
+
+        public void AddGrade(double grade)
+        {
+            if (grade <= 100 && grade >= 0)
+            {
+                    grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid value");
+            }
+        }
+
+        public Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            for (int i = 0; i < grades.Count; i++)
+            {
+                result.High = Math.Max(grades[i], result.High);
+                result.Low = Math.Min(grades[i], result.Low);
+                result.Average += grades[i];
+            }
+            result.Average /= grades.Count;
+
+            return result;
+        }
+
+        public int GetGradeCount()
+        {
+            return grades.Count;
+        }
+
+        private List<double> grades;
+        public string Name;
+    }
+}
---
+++ test/GradeBookTest/BookTests.cs
@@ -0,0 +1,41 @@
+using GradeBook;
+using System;
+using Xunit;
+
+namespace GradeBookTest
+{
+    public class BookTests
+    {
+        [Fact]
+        public void BookDoesNotAddTooHighGrades()
+        {
+            // arrange
+            var book = new Book("");
+            book.AddGrade(105);
+
+            // act
+            var count = book.GetGradeCount();
+
+            // asserts
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void BookCalculatesAnAverageGrade()
+        {
+            // arrange
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+
+            // act
+            var result = book.GetStatistics();
+
+            // assert
+            Assert.Equal(85.6, result.Average, 1);
+            Assert.Equal(90.5, result.High, 1);
+            Assert.Equal(77.3, result.Low, 1);
+        }
+    }
+}
