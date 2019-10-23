7e554eb43526db6df796b4f461c37e053f93072f
Rocangus <hunter_sniper112@hotmail.com>
2019-10-23 10:18:37 +0200

    Fundamentals 8 Defining an Event

--- GradeBook/Book.cs
+++ GradeBook/Book.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
+    public delegate void GradeAddedDelegate(object sender, EventArgs args);
+
     public class Book
     {
+
         public Book(string name)
         {
             grades = new List<double>();
@@ -38,12 +41,18 @@ namespace GradeBook
             if (grade <= 100 && grade >= 0)
             {
                     grades.Add(grade);
+                if(GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
             }
             else
             {
                 throw new ArgumentException($"Invalid {nameof(grade)}");
             }
         }
+
+        public event GradeAddedDelegate GradeAdded;
 
         public Statistics GetStatistics()
         {
